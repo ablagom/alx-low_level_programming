1566ad68a7046de93029f56fb55757bef30de423
ablagom <ablayetdsi@gmail.com>
2023-03-20 18:45:40 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * main - Prints the desired string to the standard error
+ * main - Print: "the desired string to the standard error"
  *
- * Return: one on success
+ * Return: 1 if success
  *
  */
 int main(void) {
- 	 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+ 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
   	return 1;
 }
