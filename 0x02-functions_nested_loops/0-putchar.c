cf7b059165416304ab898dbf0a24b52f48753251
ablagom <ablayetdsi@gmail.com>
2023-03-22 16:51:23 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "hello.h"
+
+/**
+ * main - Prints hello
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints hello without stdio
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{
+	_putchar('H');
+	_putchar('e');
+	_putchar('l');
+	_putchar('l');
+	_putchar('o');
+	return (0);
+}
