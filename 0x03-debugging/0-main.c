fc4effe2c616628bdfaa318e978c73043a18432d
ablagom <ablayetdsi@gmail.com>
2023-03-23 18:52:29 -0700

    more main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+/**
+* main - Test function for positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
 
-#include <stdio.h>
-
-void positive_or_negative(int i);
-
-#endif /* MAIN_H */
